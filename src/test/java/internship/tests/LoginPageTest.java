c42eb5d5a289bd6713579646cf8a73bae4be1ce9
cristina.nedin <cristina.nedin@3pillarglobal.com>
2019-07-25 12:23:11 +0300

    added credentials in config file

--- src/test/java/internship/tests/LoginPageTest.java
+++ src/test/java/internship/tests/LoginPageTest.java
@@ -1,25 +1,22 @@
 package internship.tests;
 
 import internship.framework.core.TestBase;
+import internship.framework.core.runner.GlobalProperties;
 import internship.framework.pages.Homepage;
 import internship.framework.pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginPageTest extends TestBase {
-
-    private static final String EMAIL_ADDRESS = "nicoricicllaudia@yahoo.ro";
-    private static final String PASSWORD_TEXT = "Telegraph1!";
-
-    @Test(testName = "Open Login page",
-            description = "Open page and verify login")
+    @Test (testName = "Open Login page",
+        description = "Open page and verify login")
     public void checkLoginPage() {
 
         LoginPage loginPage = new Homepage().clickLoginButton();
 
-        loginPage.enterEmail(EMAIL_ADDRESS)
-                .enterPassword(PASSWORD_TEXT)
-                .clickLogInButton();
+        loginPage.enterEmail(GlobalProperties.USERNAME_VALUE)
+            .enterPassword(GlobalProperties.PASSWORD_VALUE)
+            .clickLogInButton();
         Assert.assertTrue(loginPage.isUserLoggedIn(), "User is not logged in");
 
     }
